f273d4a75cd7dc9696f140386ced1d5ad882795a
sandy-zgf <2657434027@qq.com>
2022-09-27 13:10:06 +0800

    done

--- constThings/main.cpp
+++ constThings/main.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 
+class Apple{
+    private:
+    int people[100];
+    public:
+    Apple(int i);
+    const int apple_number;
+
+};
+
+Apple::Apple(int i):apple_number(i) {}
+
 int main(int, char **)
 {
 
